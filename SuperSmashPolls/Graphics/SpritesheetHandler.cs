c2b3168256fa737eab6cee7c40df0f8ba87c11fe
WilliamKluge <wkluge17@moundsparkacademy.org>
2016-10-05 14:33:56 -0500

    Added Graphics Template

---
+++ SuperSmashPolls/Graphics/SpritesheetHandler.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperSmashPolls {
+
+/*******************************************************************************************************************//**
+ * TODO This class
+ * This class handles spritesheets.
+ **********************************************************************************************************************/
+    class SpritesheetHandler {
+        /* The amount of time (in seconds) that it takes to cycle through the sheet */
+        int PlayTime;
+        /* The size of each item in the sheet (ie. 32-bit spritesheet is (32, 32) */
+        Point ImageSize;
+
+/*******************************************************************************************************************//**
+ * TODO This function
+ * Gives the user the desired image from the sheet.
+ * @param image The X and Y coordinate of the image to get
+ **********************************************************************************************************************/
+        Texture2D GetImage(Point image) {
+
+            return new Texture2D();
+
+        }
+
+    }
+
+}
