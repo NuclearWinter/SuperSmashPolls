170cc34180061c156aa80ca4ee3ae5ef9b42eea5
william <wkluge17@moundsparkacademy.com>
2016-10-10 17:26:30 -0500

    WorldUnit work

--- SuperSmashPolls/World Control/WorldUnit.cs
+++ SuperSmashPolls/World Control/WorldUnit.cs
@@ -16,9 +16,16 @@ namespace SuperSmashPolls.World_Control {
 
 /*******************************************************************************************************************//**
  * TODO This method
- * This gives a position on the screen based on a percentage of placement
+ * Helps to find placement of objects by giving positions on the screen.
+ * @param xRatio A number 0.0 - 1.0 representing percent of the screen to move the object over on the x-axis.
+ * @param yRatio A number 0.0 - 1.0 representing percent of the screen to move the object over on the y-axis.
+ * @return The amount of pixels to displace an object to achieve the desired ratio.
  **********************************************************************************************************************/
-        
+        public Vector2 GetScreenPosition (float xRatio, float yRatio) {
+            
+            return new Vector2(xRatio * ScreenSize.X, yRatio * ScreenSize.Y);
+
+        }
 
     }
 
