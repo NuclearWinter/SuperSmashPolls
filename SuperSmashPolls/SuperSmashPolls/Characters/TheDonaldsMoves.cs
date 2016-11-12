67b51b5fea0529b4c844ed4a9bd07f53d42d5d1a
0110111 0110111 <wkluge17@william-kluge.hsd1.mn.comcast.net>
2016-11-12 15:05:44 -0600

    Documented TheDonald's moves

--- SuperSmashPolls/SuperSmashPolls/Characters/TheDonaldsMoves.cs
+++ SuperSmashPolls/SuperSmashPolls/Characters/TheDonaldsMoves.cs
@@ -9,9 +9,19 @@ using Microsoft.Xna.Framework;
 namespace SuperSmashPolls.Characters {
 
     class TheDonaldsMoves : Moves {
+        /// <summary>
+        /// Builds a wall. This handles the creation of the body and the forces of the wall.
+        /// </summary>
+        /// <param name="character">The character casting this</param>
         public override void Special(Character character) {
             SideSpecial(character);
         }
+        /// <summary>
+        /// Creates an explosion outward from TheDonald. This is a linear explosion from the center of TheDonald's body,
+        /// with a radius of 4m and a force of 300, maxforce of the max value of a float. This explosion does not affect
+        /// TheDonald.
+        /// </summary>
+        /// <param name="character">The character casting this</param>
         public override void SideSpecial(Character character) {
 
             character.CharacterBody.CollisionGroup = Int16.MaxValue - 1;
@@ -20,14 +30,23 @@ namespace SuperSmashPolls.Characters {
                 Power = 1,
                 DisabledOnGroup = Int16.MaxValue - 1
             };
+
             Explosion.Activate(character.GetPosition(), 4, 300);
 
             character.CharacterBody.CollisionCategories = Category.All;
 
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="character">The character casting this.</param>
         public override void UpSpecial(Character character) {
             throw new NotImplementedException();
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="character">The character casting this.</param>
         public override void DownSpecial(Character character) {
             throw new NotImplementedException();
         }
