a1f2849032c6527716381bcf2bb261eac6f4586f
William Kluge <wkluge17@william-kluge.local>
2016-12-07 11:40:48 -0600

    Worked on collider bodies

--- SuperSmashPolls/SuperSmashPolls/Characters/Moves.cs
+++ SuperSmashPolls/SuperSmashPolls/Characters/Moves.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using FarseerPhysics;
+using FarseerPhysics.Common;
+using FarseerPhysics.Common.Decomposition;
 using FarseerPhysics.Common.PhysicsLogic;
+using FarseerPhysics.Common.TextureTools;
 using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using SuperSmashPolls.Graphics;
 using SuperSmashPolls.Levels;
 
@@ -17,16 +22,33 @@ namespace SuperSmashPolls.Characters {
     /// <remarks>To have audio play during a move you must run the AudioHandler.PlayEffect command</remarks>
     public abstract class Moves {
 
-        /// <summary>The audio handler to play during the special attack</summary>
-        protected internal AudioHandler SpecialSound;
-        /// <summary>The audio handler to play during the side special attack</summary>
-        protected internal AudioHandler SideSpecialSound;
-        /// <summary>The audio handler to play during the up special attack</summary>
-        protected internal AudioHandler UpSpecialSound;
-        /// <summary>The audio handler to play during the down special attack</summary>
-        protected internal AudioHandler DownSpecialSound;
-        /// <summary>The audio handler to play during a basic attack</summary>
-        protected internal AudioHandler BasicAttackSound;
+        /// <summary>Audio handlers to play during attacks</summary>
+        protected internal AudioHandler SpecialSound,
+            SideSpecialSound,
+            UpSpecialSound,
+            DownSpecialSound,
+            BasicAttackSound;
+        /// <summary>The bodies of moves. These are used to detect collisions with characters</summary>
+        protected internal List<Vertices> SpecialCollider,
+            SideSpecialCollider,
+            UpSpecialCollider,
+            DownSpecialCollider,
+            BasicAttackCollider;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="special"></param>
+        /// <param name="sideSpecial"></param>
+        /// <param name="upSpecial"></param>
+        /// <param name="downSpecial"></param>
+        /// <param name="basicAttack"></param>
+        protected internal void AssignColliderBodies(Texture2D special, Texture2D sideSpecial, Texture2D upSpecial, 
+            Texture2D downSpecial, Texture2D basicAttack) {
+            
+            
+
+        }
 
         /// <summary>
         /// Trys to play the sound effect for the special attack
@@ -63,6 +85,39 @@ namespace SuperSmashPolls.Characters {
             };
 
             Explosion.Activate(AttackPosition, ConvertUnits.ToSimUnits(30), 700);
+
+        }
+
+        /// <summary>
+        /// Creates a polygon from a texture. This is the important function here.
+        /// </summary>
+        /// <param name="texture">The texture to make a body from</param>
+        /// <param name="density">The density of the object (Will almost always be one</param>
+        /// <param name="scale">The scale of the object (how much to change its size)</param>
+        /// <param name="algorithm">The decomposition algorithm to use</param>
+        /// <remarks> Available algorithms to use are Bayazit, Dealuny, Earclip, Flipcode, Seidel, SeidelTrapazoid</remarks>
+        /// @warning In order for this to work the input must have a transparent background. I highly reccomend that you
+        /// only use this with PNGs as that is what I have tested and I know they work. This will only produce a bosy as
+        /// clean as the texture you give it, so avoid partically transparent areas and little edges.
+        public List<Vertices> CreatePolygonFromTexture(Texture2D texture, float scale, float density = 1,
+            TriangulationAlgorithm algorithm = TriangulationAlgorithm.Earclip) {
+
+            uint[] TextureData = new uint[texture.Width * texture.Height]; //Array to copy texture info into
+
+            texture.GetData<uint>(TextureData); //Gets which pixels of the texture are actually filled
+
+            Vertices vertices = TextureConverter.DetectVertices(TextureData, texture.Width);
+            List<Vertices> VertexList = Triangulate.ConvexPartition(vertices, algorithm);
+
+            Vector2 VertScale = new Vector2(ConvertUnits.ToSimUnits(scale));
+            foreach (Vertices Vert in VertexList)
+                Vert.Scale(ref VertScale); //Scales the vertices to match the size we specified
+
+            Vector2 Centroid = -vertices.GetCentroid();
+            vertices.Translate(ref Centroid);
+            //basketOrigin = -centroid;
+
+            return VertexList;
 
         }
 
